00499b07b3e32e339cf1b08f5ef2739e843799e5
Jakub Hava <jakub.hava@gmail.com>
2018-03-13 15:24:58 -0700

    [SW-722] Mojo2 integration

---
+++ ml/src/main/scala/org/apache/spark/ml/h2o/models/H2OMojoPipelineModel.scala
@@ -0,0 +1,12 @@
+package org.apache.spark.ml.h2o.models
+
+class H2OMojoPipelineModel {
+
+}
+
+object H2OMojoPipelineModel {
+
+  def createFromMojoPipeline(path: String): H2OMojoPipelineModel = {
+    null
+  }
+}
---
+++ ml/src/test/scala/org/apache/spark/ml/spark/models/H2OMojoPipelineModelTest.scala
@@ -0,0 +1,38 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.apache.spark.ml.spark.models
+
+import org.apache.spark.SparkContext
+import org.apache.spark.h2o.utils.SparkTestContext
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class H2OMojoPipelineModelTest extends FunSuite with SparkTestContext {
+
+  override def beforeAll(): Unit = {
+    sc = new SparkContext("local[*]", "test-local", conf = defaultSparkConf)
+    super.beforeAll()
+  }
+
+  test("Prediction on Mojo Pipeline (Mojo2)") {
+
+  }
+
+}
